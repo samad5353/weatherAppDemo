b626beef9ed6794846215332d77a6d8254ff4d7f
Samad <asamad5353@gmail.com>
2021-11-27 23:40:27 +0400

    various schemes added like testing staging & prod

--- weatherApp/VIPER/Utilities/URLs.swift
+++ weatherApp/VIPER/Utilities/URLs.swift
@@ -8,7 +8,15 @@
 import Foundation
 
 struct APPURL {
-    static let BaseURL                  = "https://api.openweathermap.org/data/2.5/onecall?"
+    #if Testing
+        static let BaseURL                  = "https://api.openweathermap.org/data/2.5/onecall?"
+    #elseif Staging
+        static let BaseURL                  = "https://api.openweathermap.org/data/2.5/onecall?"
+    #elseif Production
+        static let BaseURL                  = "https://api.openweathermap.org/data/2.5/onecall?"
+    #else
+        static let BaseURL                  = "https://api.openweathermap.org/data/2.5/onecall?"
+    #endif
     
     struct Weather {
         static let oneLoginForForcast = "lat=%f&lon=%f&exclude=alerts,minutely&appid=%@&units=metric"
